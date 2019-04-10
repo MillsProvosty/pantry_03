722be7ec7fbe8e1ea6d07413c103f1f0212d08aa
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-04-10 11:56:58 -0600

    UHhhhhhh. Stock_Check got me.

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -1,15 +1,16 @@
 class Pantry
   attr_reader :stock
+
   def initialize
     @stock = Hash.new(0)
   end
 
   def stock_check(ingredient_arg)
-    if @stock = {}
-      return 0
-    else
-      if @stock.include?(ingredient_arg)
-        return quantity
+    @stock.each do |key, value|
+      if key == ingredient_arg
+       value
+     else
+        return 0
       end
     end
   end
